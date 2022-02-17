bebb3584dca7b8e7b37b8d782766e1bb4013efa1
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-02-17 11:43:33 -0500

    Diseñando Tab Navigation con iconos

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -3,15 +3,51 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FavoriteScreen from '../screens/Favorite';
 import PodekexScreen from '../screens/Podekex';
 import AccountScreen from '../screens/Account';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {Image} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Favorite" component={FavoriteScreen} />
-      <Tab.Screen name="Podekex" component={PodekexScreen} />
-      <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Navigator screenOptions={{headerShown: false, showIcon: true}}>
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Podekex"
+        component={PodekexScreen}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: 'Mi cuenta',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require('../../assets/pokeball.png')}
+      style={{width: 75, height: 75, top: -15}}
+    />
   );
 }
