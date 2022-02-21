940c56034725d33123a4cbfe18c33e1fe24d3497
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-02-21 09:17:20 -0500

    Formulario Login

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,10 +1,48 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Keyboard,
+} from 'react-native';
 import React from 'react';
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        placeholder="Nombre de usuario"
+        style={styles.input}
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+
+      <Button title="Entrar" onPress={() => console.log('Entrando')} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
