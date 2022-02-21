73fee3ff1766be5725b3dbec0bcd39a275963a04
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-02-21 15:45:18 -0500

    Almacenando un Pokémon favorito con AsyncStorage

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,12 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import {addPokemonFavoriteApi, getPokemosFavoriteApi} from '../../api/favorite';
 
 export default function Favorite(props) {
   const {id} = props;
-  const addFavorite = () => {
-    console.log('añadiendo a favorite', id);
+
+  const addFavorite = async () => {
+    await addPokemonFavoriteApi(id);
   };
+
   return (
     <Icon
       name="heart"
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,7 +1,15 @@
 import {SafeAreaView, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {getPokemosFavoriteApi} from '../api/favorite';
 
 export default function Favorite() {
+  const [favorite, setFavorites] = useState();
+  useEffect(() => {
+    async () => {
+      const response = await getPokemosFavoriteApi();
+    };
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
