51961d5bf90a80097df255b01211bdbf75b0818a
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-02-22 08:38:07 -0500

    Navegación hacia atrás en favoritos

--- src/navigation/FavoriteNavigation.js
+++ src/navigation/FavoriteNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FavoriteScreen from '../screens/Favorite';
+import PokemonScreen from '../screens/Pokemon';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,14 @@ export default function FavoriteNavigation() {
         name="Favorite"
         component={FavoriteScreen}
         options={{title: 'Favoritos'}}
+      />
+      <Stack.Screen
+        name="Pokemon"
+        component={PokemonScreen}
+        options={{
+          title: '',
+          headerTransparent: true,
+        }}
       />
     </Stack.Navigator>
   );
